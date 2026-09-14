3ec4303429f64a4aae35017201985d812e7dc38b
Mike <dino.lorenzini+gh@students.victorschools.org>
2026-09-14 10:37:41 -0400

    (feat) 404 catchall card

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const env = require("dotenv");
 const app = express();
 const PORT = env.DEVELOPMENT_PORT || 8080;
+
+
+
+app.use((req, res) => {
+  res.status(404).json({"error":true,"status":404,"errorDetails":'Page not found"});
+})
 
 app.listen(PORT, () => {
   console.log("=============");
